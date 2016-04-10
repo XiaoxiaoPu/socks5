d02f81ae82ca5bdab117e3f83c52e60e86c722f9
Xiaoxiao <i@pxx.io>
2016-04-10 20:04:17 +0800

    add scripts to run clang lint, coverage test

---
+++ .coverage.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+# build libmill
+rm -rf libmill
+curl -s -L https://github.com/sustrik/libmill/archive/master.tar.gz | tar -zxf -
+mv libmill-master libmill
+cd libmill
+./autogen.sh
+./configure
+make
+rm -f $(ls .libs/* | grep -v "\.a$")
+cd ../
+
+# build with coverage
+if [ -f Makefile ]; then
+    make distclean
+fi
+autoreconf -if
+export CPPFLAGS=-I$(pwd)/libmill
+export LDFLAGS=-L$(pwd)/libmill/.libs
+export CFLAGS="-fprofile-arcs -ftest-coverage"
+./configure --enable-debug
+make
+
+# run tests
+src/socks5 -h || true
+src/socks5 -a || true
+src/socks5 -p || true
+src/socks5 -w || true
+src/socks5 --nothisoption || true
+src/socks5 -a 127.0.0.1 -p 10080 -w 4 &
+curl -vv --socks5-hostname 127.0.0.1:10080 http://github.com/
+curl -vv --socks5-hostname 127.0.0.1:10080 https://github.com/
+pkill -INT socks5
+
+# send coverage report to codecov.io
+bash <(curl -s https://codecov.io/bash)
+
+# cleanup
+make distclean
